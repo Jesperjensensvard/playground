9cc6fe160e7e9e3b97b8c0a7d5a671e5eb275d39
Jesper Jensen sverd <jesperjensensverd@Jespers-MacBook-Pro.local>
2018-10-04 14:56:44 +0200

    update4

--- main.js
+++ main.js
@@ -1,2 +1,87 @@
+ 
 
-console.log("it works")
+function changecolor() {
+
+  var bgColor = document.body.style.backgroundColor
+
+   if (bgColor == "value" ) {
+    document.body.style.backgroundColor = "blue";
+   } else {
+    document.body.style.backgroundColor = "value";
+
+   }
+
+} 
+
+
+
+    
+      function randomColor() {
+        var blue = Math.floor(Math.random() * 256);
+        var green = Math.floor(Math.random() * 256);
+        var value = Math.floor(Math.random() * 256);
+        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+     console.log(bgColor);
+      
+        document.body.style.background = bgColor;
+        }
+
+ 
+ 
+ function randomColor() {
+    var randomvalue = Math.random();
+
+     if (randomvalue > 0.8) {
+        document.body.style.backgroundColor = "value";
+     } else if (randomvalue > 0.5){
+     document.body.style.backgroundColor = "blue";
+     } 
+
+} 
+ 
+    function TotalRandomColor() {
+        var  blue  =  converttox(Math.round(Math.random()* 15));
+        var  green =  converttox(Math.round(Math.random()* 15));
+        var  red =  converttox(Math.round(Math.random()* 15));
+        var  blue2  = converttox(Math.round(Math.random()* 15));
+        var  green2 = converttox(Math.round(Math.random()* 15));
+        var  red2 =  converttox(Math.round(Math.random()* 15));
+
+        var  rgbValue = "#"+ blue + green + red +blue2 + green2 + red2;
+
+        document.body.style.background = rgbValue;
+     
+         
+}
+
+function converttox(value) {
+    if( value == 10 ){
+        return  "A" 
+    } 
+    
+    else if (value == 11){
+          return "B"        
+    }
+    
+    else if (value == 12){
+          return "C"        
+    }
+    
+    else if (value == 13){
+          return  "D"        
+    
+    }
+    
+    else if (value == 14){
+          return  "E"        
+
+    }
+    
+    else if (value == 15){
+          return  "F"        
+    }
+     else {
+        return value
+    }
+}
+ 
